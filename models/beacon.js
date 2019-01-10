02c91b8d571074c25936fac1c56d5675b4ac5f87
Twitty Manymoon <likimaru-non@hotmail.com>
2019-01-10 20:26:21 +0700

    67 beacon

---
+++ models/beacon.js
@@ -0,0 +1,24 @@
+let mongoose = require("mongoose");
+// require('mongoose-double')(mongoose);
+
+//Article Schema
+let beaconSchema = mongoose.Schema({
+    Timestamp: {
+        type: String,
+        // timestamps: true,
+        required: true
+    },
+    P_IN: {
+        type: Number,
+        required: true
+    },
+    P_OUT: {
+        type: Number,
+        required: true
+    }
+});
+
+let Beacon = (module.exports = mongoose.model(
+    "Beacon",
+    beaconSchema
+));
