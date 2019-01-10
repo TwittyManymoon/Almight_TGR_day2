3fcf3f01fa1519c19857940ccad8e99f95e05bf8
Twitty Manymoon <likimaru-non@hotmail.com>
2019-01-10 14:31:41 +0700

    46

--- app.js
+++ app.js
@@ -304,7 +304,7 @@ app.post("/receiveData", (req, res) => {
 
 app.get("/showData", (req, res) => {
 
-    db.sensors.find({}, (err, data) => {
+    Sensors.find({}, (err, data) => {
         if (err) {
             console.log(err);
             return;
